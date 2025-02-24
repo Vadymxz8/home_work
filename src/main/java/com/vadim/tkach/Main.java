38e96b5893e09ac323bd5968b3f64e437261560a
Vadymxz8 <vadim.tkach2017@gmil.com>
2025-02-24 15:04:09 +0100

    add hello world

--- src/main/java/com/vadim/tkach/Main.java
+++ src/main/java/com/vadim/tkach/Main.java
@@ -2,6 +2,6 @@ package com.vadim.tkach;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Everybody!");
+        System.out.println("Hello, World!");
     }
 }
